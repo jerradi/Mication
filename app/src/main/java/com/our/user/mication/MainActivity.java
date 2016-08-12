1a49ac5d9faeb3877c2447e86a80baf67e54ae7d
jerradi <jamal.radi.90@gmail.com>
2016-08-12 17:14:40 +0100

    Comments Added

--- app/src/main/java/com/our/user/mication/MainActivity.java
+++ app/src/main/java/com/our/user/mication/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
 
 // This methode to ensure time is syncked with user clicks
     private int countClicks(){
-        return (int) Math.random()*1000;
+        return (int) Math.random()*1000-1;
     }
 }
