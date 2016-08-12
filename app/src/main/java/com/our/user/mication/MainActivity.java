66eb84a0278019eed64a83503261a157483f86ee
jerradi <jamal.radi.90@gmail.com>
2016-08-12 21:09:28 +0000

    its was + not -

--- app/src/main/java/com/our/user/mication/MainActivity.java
+++ app/src/main/java/com/our/user/mication/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-// This methode to ensure time is syncked with user clicks
+
     private int countClicks(){
         return (int) Math.random()*1000+1;
     }
