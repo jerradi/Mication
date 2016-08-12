6e02a47ea9e58cbb131b0764309b40ecd87425f8
jerradi <jamal.radi.90@gmail.com>
2016-08-12 17:10:46 +0100

    Initial commit

---
+++ app/src/main/java/com/our/user/mication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.our.user.mication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private int countClicks(){
+        return (int) Math.random()*1000;
+    }
+}
